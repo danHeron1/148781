7b98b0e993788dc97df545da0074063271c0651e
Axel Sánchez <axelsanchez5333@gmail.com>
2020-02-06 11:07:03 -0700

    log
    clase log in

---
+++ practiquishaa_vue/src/components/LoginForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <h3>Hello, Crack</h3>
+    <h3>Welcome back homs</h3>
+    <div class="col form-group">
+      <input
+        type="email"
+        placeholder="correo"
+        class="form-control mb-2"
+      >
+      <input
+        type="pswd"
+        placeholder="contraseña"
+        class="form-control mb-2"
+      >
+      <div class="form-group">
+        <div class="row">
+          <div class="col">
+            <small>Remember Me</small>
+          </div>
+
+          <div class="col">
+
+            <small>Forgot pswrd prroo?</small>
+          </div>
+
+        </div>
+
+      </div>
+
+      <button class="btn btn-purple btn-dark btn-block">Iniciar Sesión</button>
+    </div>
+  </section>
+</template>
+<script lang="js">
+export default {
+  name: 'Login Form'
+}
+</script>
+<style lang="scss">
+.btn-purple {
+  background: rgb(243, 173, 22);
+  &:hover,
+  &:active {
+    background: red;
+  }
+}
+</style>
---
+++ practiquishaa_vue/src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="login container1">
+    <div class="card p-3">
+      <div class="row">
+        <div class="col">
+
+          <LoginForm></LoginForm>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="js">
+import LoginForm from '@/components/LoginForm'
+
+export default {
+  name: 'login',
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style lang="scss">
+.container1 {
+  width: 30%;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: 15%;
+  margin-left: auto;
+}
+</style>
